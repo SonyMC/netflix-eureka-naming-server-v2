05bb9e53f248b8b326c1068c7a262b2e6b3f18ab
Sony Mathew <mailsonymathew@gmail.com>
2021-07-17 18:46:57 +0530

    first commit

---
+++ src/test/java/com/mailsonymathew/microservices/netflixeurekanamingserverv2/NetflixEurekaNamingServerV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mailsonymathew.microservices.netflixeurekanamingserverv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
